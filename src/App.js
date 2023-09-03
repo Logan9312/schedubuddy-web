6ffae537c275e899bcabead1c13c7e8bf20786be
Logan <lgntravis@gmail.com>
2023-09-03 14:43:40 -0600

    Testing dynamic URL

--- src/App.js
+++ src/App.js
@@ -16,15 +16,27 @@ const theme = createTheme({
   },
 });
 
+const API_URL = process.env.REACT_APP_API_URL;
+const urlData = window.location.search;
+
 const App = () => (
-  <div className="page-container">
-    <ThemeProvider theme={theme}>
-      <div className="content-wrap">
-        <Main />
-      </div>
-      <Footer />
-    </ThemeProvider>
-  </div>
+  <>
+    <head>
+      <meta
+        property="og:image"
+        content={API_URL+urlData}></meta>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:image:type" content="image/png" />
+    </head>
+    <div className="page-container">
+      <ThemeProvider theme={theme}>
+        <div className="content-wrap">
+          <Main />
+        </div>
+        <Footer />
+      </ThemeProvider>
+    </div>
+  </>
 );
 
 export default App;
